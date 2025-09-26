f93f006cca30e74503c2c0b67387cd579917e447
ArthurDP-03 <arthuroliveira.c50@gmail.com>
2025-09-25 21:13:25 -0300

    Trabalho Fetch quase pronto

--- exercicios/[EX]Fetch/backend.php
+++ exercicios/[EX]Fetch/backend.php
@@ -10,6 +10,35 @@ if (!file_exists($db)) {
 
 $usuarios = json_decode(file_get_contents($db), true) ?? [];
 
-$user = $_POST["user"] ?? "Usuário inválido";
-$acao = $_POST["acao"] ?? "Ação inválida";
-?>
+if($_SERVER["REQUEST_METHOD"] == "GET") {
+    $acao = $_GET["acao"] ?? "";
+    if($acao === 'get_usuarios') {
+        echo json_encode($usuarios);
+        exit;
+    }else{
+        echo json_encode(["resposta" => "acao-invalida"]);
+    }
+}
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = $_POST["user"] ?? "";
+    $acao = $_POST["acao"] ?? "";
+    if ($acao === "login") {
+        if (in_array($user, $usuarios)) {
+            echo json_encode(["resposta" => "bem-vindo-$user"]);
+        } else {
+            echo json_encode(["resposta" => "usuario-$user-nao-cadastrado"]);
+        }
+    } else if ($acao === "cadastrar") {
+        if (in_array($user, $usuarios)) {
+            echo json_encode(["resposta" => "usuario-$user-ja-cadastrado"]);
+        } else {
+            $usuarios[] = $user;
+            file_put_contents($db, json_encode($usuarios));
+            echo json_encode(["resposta" => "ola-$user-seu-cadastro-foi-realizado"]);
+        }
+    } else {
+        echo json_encode(["resposta" => "acao-invalida"]);
+    }
+}
+
+
